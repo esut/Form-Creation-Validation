79722816a9e52901969ff936df14539e7ba24570
unknown <mounir17xm@gmail.com>
2024-07-28 23:55:57 +0100

    fix js file

--- fetch-data.js
+++ fetch-data.js
@@ -2,4 +2,8 @@ async function fetchUserData() {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContainer = document.getElementById('api-data');
 
-    
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+}
+document.addEventListener('DOMContentLoaded', fetchUserData);
